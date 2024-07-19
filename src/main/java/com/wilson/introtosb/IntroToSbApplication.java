1ed017bb060ec95a00394db86197b6dbeae663ea
Wilson Rangari <98660886+Wilsonrangari@users.noreply.github.com>
2024-07-19 23:22:26 +0200

    first commit

---
+++ src/main/java/com/wilson/introtosb/IntroToSbApplication.java
@@ -0,0 +1,31 @@
+package com.wilson.introtosb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSbApplication implements CommandLineRunner {
+
+    @Autowired
+    Apple obj;
+
+    @Autowired
+    DB db;
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IntroToSbApplication.class, args);
+
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //obj.eatApple();
+        db.getData();
+
+    }
+}
